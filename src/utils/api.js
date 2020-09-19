0e542fc3e373eda3d68648ecec1f1ad38b435abd
victor.porto <victor.porto@waybee.com.br>
2020-09-19 20:30:56 -0300

    Correções nas chamadas da API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const urlApi = {
-    value: `http://18.204.9.14:3333`
+    value: `http://100.25.34.115:3333`
 };
 
 const api = Axios.create({
@@ -15,7 +15,7 @@ api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
 
     if(token) {
-        config.headers.Authorization = token;
+        config.headers.Authorization = "Bearer " + token;
     }
 
     return config
