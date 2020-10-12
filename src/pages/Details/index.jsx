d47809961c598eba4a2065ebf86d5ca70503fca7
psteluto <psteluto@bwg.com.br>
2020-10-12 17:10:15 -0300

    Adicionado quantidade de dias na tela de detalhe

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -173,7 +173,7 @@ class Details extends Component {
                 </RowDescriptionItem>
                 <RowDescriptionItem marginBottom="10px">
                   <TextStyle color="#e73554" strong>
-                    R$ {Number(product.price).toFixed(2).replace('.', ',')}
+                    R$ {Number(product.price).toFixed(2).replace('.', ',')} por {product.max_days_location} dias
                   </TextStyle>
                 </RowDescriptionItem>
                 <RowDescriptionItem>
